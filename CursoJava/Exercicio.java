f844faad840b609038cef5033af484026816160c
Marco Antônio <163692146+marcoaccount@users.noreply.github.com>
2024-03-18 17:47:18 -0300

    teste do git

---
+++ CursoJava/Exercicio.java
@@ -0,0 +1,9 @@
+public class Exercicio {
+  public static void main(String[] args) {
+    String nome = "João";
+    String endereço = "Bairro Fulano, Rua do Ciclano";
+    Double salario = 2000.00;
+    String data = "22 de Março de 2019";
+    System.out.println("O "+nome+" mora no "+endereço+", e ganhou R$"+salario+" no dia "+data+", que impressionante!");
+  }
+}
